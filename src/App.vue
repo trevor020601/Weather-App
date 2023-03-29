3fab42a93266e6396eb2fb05e374ef824b03a10d
Trevor Karl <trevorkarl5@gmail.com>
2023-03-29 15:35:00 -0500

    Weather and Location Box Styling

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search...">
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">
+            Baton Rouge, LA
+          </div>
+          <div class="date">
+            Wednesday 29 March 2023
+          </div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">
+            59°F
+          </div>
+          <div class="weather">
+            Cloudy
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -42,5 +62,78 @@ export default {
     min-height: 100vh;
     padding: 25px;
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  }
+
+  .search-box {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  .search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+
+    color: #313131;
+    font-size: 20px;
+
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+  }
+
+  .search-box .search-bar:focus {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 16px 0px 16px 0px;
+  }
+
+  .location-box .location{
+    color:#FFF;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+  }
+
+  .location-box .date{
+    color:#FFF;
+    font-size: 20px;
+    font-weight: 300;
+    font-style: italic;
+    text-align: center;
+  }
+
+  .weather-box {
+    text-align: center;
+  }
+
+  .weather-box .temp {
+    display: inline-block;
+    padding: 10px 25px;
+    color: #FFF;
+    font-size: 102px;
+    font-weight: 900;
+
+    text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 16px;
+    margin: 30px 0px;
+
+    box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  }
+
+  .weather-box .weather {
+    color: #FFF;
+    font-size: 48px;
+    font-weight: 700;
+    font-style: italic;
+    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
   }
 </style>
